0776040f801911c1d1bbe3027486e5f8b09b7dad
ota200 <aniscratch@gmail.com>
2022-03-07 21:21:10 -0500

    First parts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [input, setInput] = useState({
+    words:"",
+    dates:"",
+    times:""
+  })
+
+  const [array,setArray] = useState([])
+
+  function getValues(e) {
+    const value = e.target.value
+    setInput({
+      ...input,
+      [e.target.id]: value
+    })
+  }
+
+  function submit(e) {
+    e.preventDefault() 
+    setArray( array=> [ ...array,[input.words,input.dates,input.times]])
+  }
   return (
     <div>
-      <h1>NOTES APP</h1>
+      <div>
+        <form onSubmit={e => submit(e)}>
+          <input id="words" onChange={e=>getValues(e)} required></input>
+
+          <input id="dates" type="date" onChange={e=>getValues(e)} required></input>
+
+          <input id="times" type="time" onChange={e=>getValues(e)}  ></input>
+
+
+          <label>
+
+          </label>
+
+        </form>
+      </div>
+      <div>
+
+        <p>{input.words}</p>
+        <p>{input.dates}</p>
+        <p>{input.times}</p>
+        <div>
+          {array.map((arra) =>
+              <li ><a key={arra} id={arra}>{arra}</a></li>
+          )}
+        </div>
+
+
+      </div>
     </div>
   );
 }
