43404fbb9e97e5ea34225a08ee3351cd28285642
ota200 <aniscratch@gmail.com>
2022-03-10 21:03:39 -0500

    save all

--- src/App.js
+++ src/App.js
@@ -1,137 +1,61 @@
 import './App.css';
 import { useState } from 'react';
 
-function App() {
-
-
-
-  //const [array,setArray] = useState([])
-  const [words,setWords] = useState("")
-  const [dates,setDates] = useState("")
-  const [times,setTimes] = useState("")
-
-  const [input, setInput] = useState([])
-
-
-
-
-  function submit(e) {
-    e.preventDefault() 
-
-    setInput( input=> [ ...input,[words,dates,times]])
-
-    console.log(input)
-  }
-
-  function getDates(e) {
-    const value = e.target.value
-    setDates(value)
-
-  }  
+export default function Notes() {
   
-  function getWords(e) {
-    const value = e.target.value
-    setWords(value)
+  const [addNote,setAddNote] = useState([])
 
-  }
-  
-  function getTimes(e) {
-    const value = e.target.value
-    setTimes(value)
-    
-  }
+  const [dates,setDate] = useState("")
+  const [words,setWord] = useState("")
+  const [times,setTime] = useState("")
 
-  function del(e) {
-    const b = e.target.id
-    console.log(input)
-    console.log(b)
 
-    const newList = input.filter((arra) => arra !== b)
-    setInput(newList)
+  function getValue(e) {
+    setWord(e.target.value)
   }
 
-  /*function (e) {
-    //setArray( array=> [ ...array,[input.words,input.dates,input.times]])
-    setInput( input=> [ ...input,input])
-    
-  }*/
+  function getText(e) {
+      e.preventDefault();
+      setAddNote([
+          ...addNote,
+          {
+              id: addNote.length,
+              name: words
+          }
+      ])
+      setWord("")
+  }
 
-  /*function del(e) {
-    
-    //console.log(e.target.id)
-    const a = e.target.id
-    const b = e.target.name
+  function removeNote(id) {
+      const newList = addNote.filter((notes) => notes.id !== id);
+      setAddNote(newList);
+  }
 
-    console.log(array[a[b]])
-    //setArray(array.filter((arra) => arra !== b))
-  }*/
-/*
-        <div>
-          {array.map((items, inx) =>{
-            return (
-              <div>
-                {items.map((subItems,ix) =>{
-                  return (
-                    <>
-                    <li ><a key={inx}>Name: {subItems} Where in array: {inx} Where in sub array: {ix}</a></li>
-                    </>
-                  )
-                })}
-                <a id={inx} onClick={e=>del(e)}>X</a>
+  function delList() {
+      setAddNote([])
+  }
 
-              </div>
-              
-            )
-          })}
-        </div>
-          {array}
-*/
   return (
-    <div>
       <div>
-        <form onSubmit={e => submit(e)}>
-          <input id="words" onChange={e=>getWords(e)} required></input>
+          <form onSubmit={getText}>
+              <input value={words} onChange={getValue} ></input>
+              <input type="date" value={words} onChange={getValue} ></input>
+              <input type="time" value={words} onChange={getValue} ></input>
 
-          <input id="dates" type="date" onChange={e=>getDates(e)} required></input>
+          </form>
+          
+          {addNote.map(notes =>(
+              <>
+                  <h1 key={notes.id} id={notes.id} >{notes.name}</h1>
+                  <button onClick={() => removeNote(notes.id)}>Click to del</button>
 
-          <input id="times" type="time" onChange={e=>getTimes(e)}  ></input>
+              </>
+          ))}
 
-
-          <label>
-
-          </label>
-
-        </form>
-      </div>
-      <div>
-
-        <p>{words}</p>
-        <p>{dates}</p>
-        <p>{times}</p>
-
-        <div>
-          {input.map((items, inx) =>{
-            return (
-              <div>
-                {items.map((subItems,ix) =>{
-                  return (
-                    <>
-                    <li key={subItems}>{subItems}</li>
-                    <a>X</a>
-                    </>
-                  )
-                })}
-                
-                <button id={items} onClick={e => del(e)}>DEL</button>
-          </div>
-              
-            )
-          })}
-        </div>
+          <button onClick={delList}>Delate the whole list</button>
 
       </div>
-    </div>
   );
 }
 
-export default App;
+
